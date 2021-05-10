48b12159480608615bf018fb5f15894c8558a7bb
Woj PL <s16496@pjwstk.edu.pl>
2021-05-10 16:55:56 +0200

    BMI displayed

--- app/src/main/java/com/example/kotlinbmi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kotlinbmi/ui/home/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.example.kotlinbmi.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -14,6 +18,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -22,9 +27,25 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val button: Button = root.findViewById(R.id.calculate)
+        val weightEdit: EditText = root.findViewById(R.id.weight)
+        val heightEdit: EditText = root.findViewById(R.id.height)
+        val result: TextView = root.findViewById(R.id.textBmi)
+        val male: RadioButton = root.findViewById(R.id.radioMale)
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val weight : Float = weightEdit.text.toString().toFloat()
+                val height : Float = heightEdit.text.toString().toFloat()
+                val bmi : Float = (weight / ((height*height) / 100))*100;
+                
+
+                result.text = "Your BMI: " + "%.1f".format(bmi).toDouble().toString() + "\nYou can eat " + male.isChecked.toString()
+            }
+        })
+
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
         })
         return root
     }
